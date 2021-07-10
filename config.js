7e13cfff7a3497759152878bc564fdf2858b8c7c
Capoo111 <87234208+Capoo111@users.noreply.github.com>
2021-07-10 21:06:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Em bé à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Em có yêu a honggg 😙',
+    title: 'Em có yêu bubu honggg 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Có yêuuu <33',
     btnNo: 'Ai thèm yêuuu :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Tại vì anh đẹp trai hihihii <33333333',
+    reply: 'Tại vì bubu đẹp trai hihihi <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-     messLink: 'http://fb.com/nam.capoo/'
+    btnAccept: 'Okiiiii lun <3',
+     messLink: 'https://www.facebook.com/nam.capoo/'
 }
